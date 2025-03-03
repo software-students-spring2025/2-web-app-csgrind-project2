50ce3204957dbd5d0acb15e4b8ecb7de1a2e8f07
maxlmeyring <max.luetkemeyring@nyu.edu>
2025-03-03 17:51:35 -0500

    finalized pages being added

--- app/routes.py
+++ app/routes.py
@@ -22,51 +22,46 @@ def page3():
 def page4():
     return render_template("page4.html")
 
-#login, method POST
-@main.route("/login", methods=["POST"])
-def login():
-    pass
+# ### -------------- Student Route -------------- ###
 
-### -------------- Student Route -------------- ###
+# #student: post lost item, method POST
+# @main.route("/lost_items", methods=["POST"])
+# def post_lost_item():
+#     pass
 
-#student: post lost item, method POST
-@main.route("/lost_items", methods=["POST"])
-def post_lost_item():
-    pass
+# #student: search for lost item, method GET
+# @main.route("/search", methods=["GET"])
+# def search_lost_item():
+#     pass
 
-#student: search for lost item, method GET
-@main.route("/search", methods=["GET"])
-def search_lost_item():
-    pass
+# #student: upload picture to lost item, method POST
+# @main.route("/lost_items/upload_picture", methods=["POST"])
+# def upload_picture():
+#     pass
 
-#student: upload picture to lost item, method POST
-@main.route("/lost_items/upload_picture", methods=["POST"])
-def upload_picture():
-    pass
+# ### -------------- Security Officer Route -------------- ###
 
-### -------------- Security Officer Route -------------- ###
+# #security officer: post found item, method POST
+# @main.route("/found_items", methods=["POST"])
+# def post_found_item():
+#     pass
 
-#security officer: post found item, method POST
-@main.route("/found_items", methods=["POST"])
-def post_found_item():
-    pass
+# #security officer: update item post, method PUT
+# @main.route("/found_items/<item_id>", methods=["PUT"])
+# def update_item_post(item_id):
+#     pass
 
-#security officer: update item post, method PUT
-@main.route("/found_items/<item_id>", methods=["PUT"])
-def update_item_post(item_id):
-    pass
+# #security officer: mark item as recovered, method DELETE
+# @main.route("/found_items/<item_id>", methods=["DELETE"])
+# def mark_item_recovered(item_id):
+#     pass
 
-#security officer: mark item as recovered, method DELETE
-@main.route("/found_items/<item_id>", methods=["DELETE"])
-def mark_item_recovered(item_id):
-    pass
+# #security officer: view lost items, method GET
+# @main.route("/lost_items/all", methods=["GET"])
+# def view_lost_items():
+#     pass
 
-#security officer: view lost items, method GET
-@main.route("/lost_items/all", methods=["GET"])
-def view_lost_items():
-    pass
-
-#security officer: restrict access
-@main.route("/restrict-access", methods=["GET"])
-def restrict_access():
-    pass
+# #security officer: restrict access
+# @main.route("/restrict-access", methods=["GET"])
+# def restrict_access():
+#     pass
