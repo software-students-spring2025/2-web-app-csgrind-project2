01c405396226ba981dc13cce806a351ad5c5a6bb
larryyang04 <larryyang2848@gmail.com>
2025-03-03 23:47:17 -0500

    fixed routes

--- app/routes.py
+++ app/routes.py
@@ -5,23 +5,31 @@ from app import app, mongo
 def home():
     return render_template("index.html")
 
-@app.route("/page1")
-def page1():
-    return render_template("page1.html")
+@app.route("/lost_items")
+def lost_items():
+    return render_template("lost_items.html")
 
-@app.route("/page2")
-def page2():
-    return render_template("page2.html")
+@app.route("/found_items")
+def found_items():
+    return render_template("found_items.html")
 
-@app.route("/page3")
-def page3():
-    return render_template("page3.html")
+@app.route("/post_item")
+def post_item():
+    return render_template("post_item.html")
 
-@app.route("/page4")
-def page4():
-    return render_template("page4.html")
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
-@app.route("/lost_items", methods=["GET"])
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/account")
+def account():
+    return render_template("account.html")
+
+@app.route("/api/lost_items", methods=["GET"])
 def get_lost_items():
     try:
         items_collection = mongo.db.item
@@ -34,11 +42,11 @@ def get_lost_items():
         print(f"Error accessing MongoDB: {e}")
         return jsonify({"error": "Database error"}), 500
 
-@app.route("/found_items", methods=["GET"])
+@app.route("/api/found_items", methods=["GET"])
 def get_found_items():
     try:
         items_collection = mongo.db.item
-        found_items = list(items_collection.find({"status": "lost"}))
+        found_items = list(items_collection.find({"status": "found"}))
 
         for item in found_items:
             item["_id"] = str(item["_id"])
@@ -46,3 +54,6 @@ def get_found_items():
     except Exception as e:
         print(f"Error accessing MongoDB: {e}")
         return jsonify({"error": "Database error"}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
